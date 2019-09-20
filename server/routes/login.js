694ba0dfb71944a232ab67d3975509a78f4f19bb
Nuria Romero <anromercas@gmail.com>
2019-09-20 10:11:56 +0200

    creacion de un segundo login para la parte de la app movil sin 2FA

--- server/routes/login.js
+++ server/routes/login.js
@@ -179,10 +179,56 @@ app.get('/login/renuevatoken', (req, res) => {
 
 });
 
+// ===========================================
+// Login de usuario sin OTP
+// ===========================================
+app.post('/loginApp', (req, res) => {
+
+    let body = req.body;
+
+    Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: '(Usuario) o contraseña incorrectos'
+                }
+            });
+        }
+
+        if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario o (contraseña) incorrectos'
+                }
+            });
+        }
+
+        let token = jwt.sign({
+            usuario: usuarioDB
+        }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN }); // caducidad en middleware autenticacion
+
+        return res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token
+        });
+    });
+
+});
+
 
 
 // ===========================================
-// Login de usuario
+// Login de usuario con OTP
 // ===========================================
 app.post('/login', (req, res) => {
 
